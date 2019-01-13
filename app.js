23267140948c4492ebc245567fbd0c464201c126
Nick Foden <nickfoden@gmail.com>
2019-01-12 23:55:09 -0500

    Adding an event

--- app.js
+++ app.js
@@ -5,30 +5,54 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
-type RootQuery{
-    events: [String!]!
-}
-type RootMutation{
-    createEvent(name:String):String
-}
-schema {
-    query:RootQuery
-    mutation:RootMutation
-}
+    type Event {
+        _id: ID!
+        title: String!
+        description:String!
+        price: Float!
+        date: String!
+    }
+
+    input EventInput {
+        title: String!
+        description:String!
+        price: Float!
+        date: String! 
+    }
+
+    type RootQuery{
+        events: [Event!]!
+    }
+    type RootMutation{
+        createEvent(eventInput: EventInput):Event
+    }
+    schema {
+        query:RootQuery
+        mutation:RootMutation
+    }
 `),
     rootValue: {
       events: () => {
-        return ["Cooking", "Sailing", "Coding"];
+        return events;
       },
       createEvent: args => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
       }
     },
     graphiql: true
